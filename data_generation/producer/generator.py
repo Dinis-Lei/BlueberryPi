40e8ea6e89e082146f28a3dc8b1a8b34da38b6d5
AfonsoCampos971 <afonso.campos@ua.pt>
2021-12-22 17:40:47 +0000

    Changes to data generation to make sensors more dynamic and implement time control

--- data_generation/producer/generator.py
+++ data_generation/producer/generator.py
@@ -4,38 +4,31 @@ import time
 import pika
 
 class Sensor:
-    def __init__(self, local, sensor_type, funcgood, funcbad) -> None:
+    def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None) -> None:
         self.local = local
         self.sensor_type = sensor_type
         self.funcgood = funcgood
         self.funcbad = funcbad
         self.alert = False
-        self.prob = 5
-        self.start_prob = [5, 15]
+        self.prob = start_prob[0]
+        self.start_prob = start_prob
+        self.prob_delta = prob_delta
+        self.value = value
 
     def change_state(self):
         r = random.randint(0,100)
         if r < self.prob:
             self.alert = not self.alert
             self.prob = self.start_prob[self.alert]
+            #print("!!!!!!!!! CHANGED STATE !!!!!!!!!!", self.alert)
         else:
-            self.prob += 1
+            self.prob += self.prob_delta[self.alert]
 
-    def generate(self):
-        self.funcgood() if self.alert else self.funcbad()
-
-
-    def __str__(self) -> str:
-        pass
-
-
-
-
-def generate_temperature_storage():
-    temp = random.uniform(-1,1)
-    timestamp = int(time.time())
-    print(timestamp ,temp)
-    return json.dumps({"key": "storage_temperature", "timestamp": timestamp, "temp": temp })
+    def generate(self, channel):
+        self.funcgood(self) if not self.alert else self.funcbad(self)
+        msg = json.dumps({"key": "plantation_temperature", "timestamp": int(time.time()), "temp": self.value, "location": self.local})
+        channel.basic_publish(exchange='', routing_key='blueberry', body=msg)
+        self.change_state()
 
 if __name__ == "__main__":
     print("Start GENERATOR")
@@ -53,8 +46,19 @@ if __name__ == "__main__":
 
     channel.queue_declare(queue='blueberry')
 
+    count = 0
     while 1:
-        time.sleep(5)
-        channel.basic_publish(exchange='', routing_key='blueberry', body=generate_temperature_storage())
+        time.sleep(0.5) # ajustar para acelerar/desacelerar o tempo (testing purposes)
+        # geradores segundo a segundo
+
+        if count % 60 == 0:
+            # geradores minuto a minuto
+
+            pass
+        elif count % 3600 == 0:
+            # geradores hora a hora
+            
+            pass
+        count += 1
 
     connection.close()
