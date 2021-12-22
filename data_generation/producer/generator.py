d20ad3a142a912aec311ebf1c47cbe7f962b5c76
AfonsoCampos971 <afonso.campos@ua.pt>
2021-12-22 21:40:23 +0000

    pH generator working with very frequent alerts (testing purposes)

--- data_generation/producer/generator.py
+++ data_generation/producer/generator.py
@@ -2,6 +2,9 @@ import random
 import json
 import time
 import pika
+from numpy.random import default_rng
+
+rng = default_rng()
 
 class Sensor:
     def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None) -> None:
@@ -30,6 +33,12 @@ class Sensor:
         channel.basic_publish(exchange='', routing_key='blueberry', body=msg)
         self.change_state()
 
+def generate_ph(sensor):
+    sensor.value = rng.normal(5, 0.4)
+
+def generate_ph_alert(sensor):
+    sensor.value = rng.normal(5, 0.9)
+
 if __name__ == "__main__":
     print("Start GENERATOR")
     connection = None
@@ -46,18 +55,32 @@ if __name__ == "__main__":
 
     channel.queue_declare(queue='blueberry')
 
+    ph_Guarda = Sensor("Guarda","ph",generate_ph,generate_ph_alert,[0,0],[10,30],5)
+    ph_Minho = Sensor("Minho","ph",generate_ph,generate_ph_alert,[0,0],[10,30],5)
+    ph_VilaReal = Sensor("Vila Real","ph",generate_ph,generate_ph_alert,[0,0],[10,30],5)
+
     count = 0
     while 1:
-        time.sleep(0.5) # ajustar para acelerar/desacelerar o tempo (testing purposes)
+        time.sleep(1/(3600 * 24)) # ajustar para acelerar/desacelerar o tempo (testing purposes)
         # geradores segundo a segundo
 
         if count % 60 == 0:
             # geradores minuto a minuto
 
             pass
-        elif count % 3600 == 0:
+        if count % 3600 == 0:
             # geradores hora a hora
             
+            pass
+        if count % (3600 * 6) == 0:
+            # geradores 6 em 6 horas
+
+            pass
+        if count % (3600 * 24) == 0:
+            # geradores diários
+            ph_Guarda.generate(channel)
+            ph_Minho.generate(channel)
+            ph_VilaReal.generate(channel)
             pass
         count += 1
 
