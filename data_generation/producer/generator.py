ca031d3d8f96dd8a9403dd093516f6ca7fa6029d
AfonsoCampos971 <afonso.campos@ua.pt>
2022-01-04 17:33:08 +0000

    Sensor timing overhaul

--- data_generation/producer/generator.py
+++ data_generation/producer/generator.py
@@ -1,10 +1,13 @@
+from numpy.random import default_rng
 import random
 import json
 import time
 import pika
 
+rng = default_rng()
+
 class Sensor:
-    def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None) -> None:
+    def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None, step=0) -> None:
         self.local = local
         self.sensor_type = sensor_type
         self.funcgood = funcgood
@@ -14,6 +17,8 @@ class Sensor:
         self.start_prob = start_prob
         self.prob_delta = prob_delta
         self.value = value
+        self.ts = 0
+        self.step = step
 
     def change_state(self):
         r = random.randint(0,100)
@@ -26,9 +31,10 @@ class Sensor:
 
     def generate(self, channel):
         self.funcgood(self) if not self.alert else self.funcbad(self)
-        msg = json.dumps({"key": self.sensor_type, "timestamp": int(time.time()), "temp": self.value, "location": self.local})
+        msg = json.dumps({"key": self.sensor_type, "timestamp": self.ts, "temp": self.value, "location": self.local})
         channel.basic_publish(exchange='', routing_key='blueberry', body=msg)
         self.change_state()
+        self.ts += self.step
 
 def generate_unit_loss(sensor):
     pass
@@ -88,27 +94,16 @@ if __name__ == "__main__":
 
     channel.queue_declare(queue='blueberry')
 
-    count = 0
-    while 1:
+    sensors = []
+
+    curr_time = 0
+    while 1:        
+        big = curr_time
+        for sensor in sensors:
+            if sensor.ts <= curr_time:
+                sensor.generate()
+                if sensor.ts > big: big = sensor.ts
+        curr_time = big
         time.sleep(1) # ajustar para acelerar/desacelerar o tempo (testing purposes)
-        # geradores segundo a segundo
-
-        if count % 60 == 0:
-            # geradores minuto a minuto
-
-            pass
-        if count % 3600 == 0:
-            # geradores hora a hora
-            
-            pass
-        if count % (3600 * 6) == 0:
-            # geradores 6 em 6 horas
-
-            pass
-        if count % (3600 * 24) == 0:
-            # geradores diários
-
-            pass
-        count += 1
 
     connection.close()
