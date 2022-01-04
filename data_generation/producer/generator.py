0e2c9a997853ae37a0677c7797e300c3438dd9f0
AfonsoCampos971 <afonso.campos@ua.pt>
2022-01-04 17:57:36 +0000

    updated

--- data_generation/producer/generator.py
+++ data_generation/producer/generator.py
@@ -1,13 +1,13 @@
+from numpy.random import default_rng
 import random
 import json
 import time
 import pika
-from numpy.random import default_rng
 
 rng = default_rng()
 
 class Sensor:
-    def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None) -> None:
+    def __init__(self, local, sensor_type, funcgood, funcbad, start_prob=[5,15], prob_delta=[1,1], value=None, step=0) -> None:
         self.local = local
         self.sensor_type = sensor_type
         self.funcgood = funcgood
@@ -17,6 +17,8 @@ class Sensor:
         self.start_prob = start_prob
         self.prob_delta = prob_delta
         self.value = value
+        self.ts = 0
+        self.step = step
 
     def change_state(self):
         r = random.randint(0,100)
@@ -29,9 +31,10 @@ class Sensor:
 
     def generate(self, channel):
         self.funcgood(self) if not self.alert else self.funcbad(self)
-        msg = json.dumps({"key": "plantation_temperature", "timestamp": int(time.time()), "temp": self.value, "location": self.local})
+        msg = json.dumps({"key": self.sensor_type, "timestamp": self.ts, "temp": self.value, "location": self.local})
         channel.basic_publish(exchange='', routing_key='blueberry', body=msg)
         self.change_state()
+        self.ts += self.step
 
 def generate_temperature(sensor):
     delta = rng.normal(0, 0.1)
@@ -60,25 +63,21 @@ if __name__ == "__main__":
     channel = connection.channel()
 
     channel.queue_declare(queue='blueberry')
-    
-    plant_temp_Guarda = Sensor("Guarda","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19)
-    plant_temp_Minho = Sensor("Minho","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19)
-    plant_temp_VilaReal = Sensor("Vila Real","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19)
 
-    count = 0
-    while 1:
-        time.sleep(1/60) # para efeitos de teste, 1 segundo em tempo real corresponderá a um minuto simulado pelo gerador!
-        # geradores segundo a segundo
-        
-        if count % 60 == 0:
-            # geradores minuto a minuto
-            plant_temp_Guarda.generate(channel) 
-            plant_temp_Minho.generate(channel)
-            plant_temp_VilaReal.generate(channel)
-            pass
-        elif count % 3600 == 0:
-            # geradores hora a hora
-            pass
-        count += 1
+    sensors = []
+
+    sensors.append(Sensor("Guarda","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19,60))
+    sensors.append(Sensor("Minho","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19,60))
+    sensors.append(Sensor("Vila Real","plantation_temperature",generate_temperature,generate_temperature_alert,[0,0],[0.2,0.9],19,60))
+
+    curr_time = 0
+    while 1:        
+        big = curr_time
+        for sensor in sensors:
+            if sensor.ts <= curr_time:
+                sensor.generate()
+                if sensor.ts > big: big = sensor.ts
+        curr_time = big
+        time.sleep(1) # ajustar para acelerar/desacelerar o tempo (testing purposes)
 
     connection.close()
