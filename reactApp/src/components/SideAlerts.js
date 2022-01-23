3170f7b7b312f2500bebd83697c2ec153feae172
Dinis-Lei <dinislei@ua.pt>
2022-01-23 23:08:34 +0000

    fixed side alerts

--- reactApp/src/components/SideAlerts.js
+++ reactApp/src/components/SideAlerts.js
@@ -1,53 +1,77 @@
 import React, { useEffect, useState } from "react"
 import Toast from 'react-bootstrap/Toast'
 import ToastContainer from 'react-bootstrap/ToastContainer'
+import { useParams } from "react-router-dom"
 import { fetchData, processString } from "../App"
 
-const getAlerts = (JSONData) => {
 
+const checkAlert = (alert) => {
+    alert.seen = true
+    const requestOptions = {
+        method: 'Put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(alert)
+    };
+    fetch('https://localhost:8080/api/alerts', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+}
+
+const getDate = (start) => {
+    let start_Date = new Date(start * 1000);
+    console.log(start)
+    let start_date = start_Date.getDate()+
+        "/"+(start_Date.getMonth()+1)+
+        "/"+start_Date.getFullYear()+
+        " "+start_Date.getHours()+
+        ":"+start_Date.getMinutes()+
+        ":"+start_Date.getSeconds();
+    return start_date
+}
+
+const stringfyAlert = (alert) => {
     let ret = {}
   
-    for (let elem in JSONData) {
-        
-        let alert = JSONData[elem];
+    
 
-        let location = alert["location"];
+    let location = alert["location"];
+    console.log(alert.sensor)
+    let sensor = processString(alert["sensor"]);
+    let val = alert["val"];
 
-        let sensor = processString(alert["sensor"]);
-        let val = alert["val"];
+    let start = alert["start"];
+    let start_Date = new Date(start * 1000);
+    let start_date = start_Date.getDate()+
+        "/"+(start_Date.getMonth()+1)+
+        "/"+start_Date.getFullYear()+
+        " "+start_Date.getHours()+
+        ":"+start_Date.getMinutes()+
+        ":"+start_Date.getSeconds();
 
-        let start = alert["start"];
-        let start_Date = new Date(start * 1000);
-        let start_date = start_Date.getDate()+
-            "/"+(start_Date.getMonth()+1)+
-            "/"+start_Date.getFullYear()+
-            " "+start_Date.getHours()+
-            ":"+start_Date.getMinutes()+
-            ":"+start_Date.getSeconds();
+    let end = alert["end"];
+    let end_Date = new Date(end * 1000);
+    let end_date = end_Date.getDate()+
+        "/"+(end_Date.getMonth()+1)+
+        "/"+end_Date.getFullYear()+
+        " "+end_Date.getHours()+
+        ":"+end_Date.getMinutes()+
+        ":"+end_Date.getSeconds();
 
-        let end = alert["end"];
-        let end_Date = new Date(end * 1000);
-        let end_date = end_Date.getDate()+
-            "/"+(end_Date.getMonth()+1)+
-            "/"+end_Date.getFullYear()+
-            " "+end_Date.getHours()+
-            ":"+end_Date.getMinutes()+
-            ":"+end_Date.getSeconds();
+    let alert_str = sensor + ": " + val + ", since: " + start_date + ", until: " + end_date;
 
-        let alert_str = sensor + ": " + val + ", since: " + start_date + ", until: " + end_date;
-  
-        if (ret[location] != undefined) {
-            ret[location] = ret[location].concat([alert_str]);
-        }
-        else {
-            ret[location] = [alert_str];
-        }
-  
+    if (ret[location] != undefined) {
+        ret[location] = ret[location].concat([alert_str]);
     }
+    else {
+        ret[location] = [alert_str];
+    }
+  
+    
     console.log(ret)
     return ret;
-  
 }
+
+
 
 const getLocations = (JSONData) => {
     let ret = [];
@@ -65,8 +89,8 @@ const SideAlerts = props => {
 
     const [show, setShow] = React.useState(true);
     const toggleShow = () => setShow(!show);
-    const [alerts_dict, setAlertsDict] = useState({});
-    const [locations_lst, setLocationsLst] = useState([]);
+    const [alerts_dict, setAlertsDict] = useState([]);
+    const {location} = useParams();
 
     /* alerts_dict = {
         location1: [
@@ -95,8 +119,10 @@ const SideAlerts = props => {
 
         let alerts_data = fetchData("alerts");
         alerts_data.then(function(result){
-            setAlertsDict(getAlerts(result));
-            setLocationsLst(getLocations(result));
+            console.log("BBBBB")
+            console.log(result)
+            setAlertsDict(result);
+            //setLocationsLst(getLocations(result));
         });
 
     }, []);
@@ -122,20 +148,28 @@ const SideAlerts = props => {
                     </Toast.Header>
                     <Toast.Body>
                     {
-                        locations_lst.map(
-                            (location) => {
+                        
+                        
+                        alerts_dict.map(
+                            (alert) => {
+                                console.log("AAAAA")
+                                console.log(alert)
+                                console.log(location)
+                                if(alert.location != location){
+                                    return (<div></div>);
+                                }
                                 return(
-                                    alerts_dict[location].map(
-                                        (alert) => {
-                                            console.log(alert)
-                                            return(
-                                                <p>{alert}</p>
-                                            )
-                                        }
-                                    )
+                                    !alert.seen && 
+                                    <div>
+                                        <p>{processString(alert.sensor) + ": " + alert.val + ", since: " + getDate(alert.start) + ", until: " + getDate(alert.end)}</p>
+                                        <button onClick={checkAlert(alert)}></button>
+                                    </div>
                                 )
                             }
                         )
+                        
+                            
+                        
                     }
                     </Toast.Body>
                 </Toast>
