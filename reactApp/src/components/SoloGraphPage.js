e17a41f38208bb462a4b1bbc8994e1591e473319
Dinis-Lei <dinislei@ua.pt>
2022-01-24 22:29:42 +0000

    date filter

--- reactApp/src/components/SoloGraphPage.js
+++ reactApp/src/components/SoloGraphPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import CanvasJSReact from '../canvasjs.react';
 import { fetchData, processString } from "../App";
 import { useParams } from "react-router-dom"
+import HourPicker from "./HourPicker";
+import DatePicker from "./DatePicker";
+import { Button } from "react-bootstrap";
 import RangeSlider from 'react-bootstrap-range-slider';
 import { Form, Col, Row } from "react-bootstrap";
 
@@ -29,6 +32,35 @@ const process_date = (timestamp) => {
     return date_str;
 }
 
+export const getDataPoints = (JSONData) => {
+    let ret = [];
+    let counter = 1;
+    let graphData;
+
+    console.log(JSONData)
+
+    if (JSONData.length < 1){
+        return 
+    }
+
+    if (JSONData.length < 20) {
+        graphData = JSONData;
+    }
+    else {
+        let firstElem = 0; 
+        let lastElem = 40;
+        graphData = JSONData.slice(firstElem, lastElem); // não inclui lastElem
+    }
+    graphData.reverse()
+    for (const dataPoint of graphData) {
+        let newElem = {x: counter, y: dataPoint["data"], label: process_date(dataPoint["timestamp"])};
+        ret.push(newElem);
+        counter += 1;
+    }
+
+    return ret;
+}
+
 
 const SoloGraphPage = props =>{
 
@@ -36,7 +68,12 @@ const SoloGraphPage = props =>{
     const [graphTitle, setGraphTitle] = useState("[no title]");
     const { location } = useParams()
     const [flg, setFlg] = useState(true);
-    const { sensor } = useParams();
+    const { sensor } = useParams(); 
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [startHour, setStartHour] = useState('00:00:00');
+    const [endHour, setEndHour] = useState('00:00:00');
+    
     const [ nDataPoints, setnDataPoints ] = useState(100); 
     const [ maxDataPoints, setMaxDataPoints] = useState(200);
     // const [times, setTimes] = useState([]);
@@ -85,10 +122,7 @@ const SoloGraphPage = props =>{
         let data = fetchData(url); // data is a promise object
         data.then(function (result) {
             setMyDataPoints(getDataPoints(result));
-            //console.log(`${dataType}`)
-            //console.log(myDataPoints)
             setGraphTitle(processString(sensor));
-            // setTimes(getTimes(result));
         });
 
     }, [flg, nDataPoints]);
@@ -108,7 +142,6 @@ const SoloGraphPage = props =>{
         },
         axisX: {
             title: "Time",
-            //prefix: "W",
             interval: 2
         },
         data: [{
@@ -119,11 +152,50 @@ const SoloGraphPage = props =>{
         }]
     }
 
-    return (
+    const filterByDate = () => {
+        let url = location + "/" + sensor + "?";
+        if(startDate != ''){
+            url += "start=" + startDate + "-" + startHour + "&";
+        }
+        if(endDate != ''){
+            url += "end=" + endDate + "-" + endHour
+        }
+
+        console.log(url)
+
+        let data = fetchData(url); // data is a promise object
+        data.then(function (result) {
+            console.log(result)
+            setMyDataPoints(getDataPoints(result));
+        });
+
+    }
+
+    return (	
         <div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            <label>Start date:</label>
+                            <DatePicker onChange={setStartDate} />
+                            <HourPicker onChange={setStartHour} />
+                        </td>
+                        <td>
+                            <label>End date:</label>
+                            <DatePicker onChange={setEndDate} />
+                            <HourPicker onChange={setEndHour}/>
+                        </td>
+                    </tr>
+                    <Button onClick={filterByDate}>Filter</Button>
+                </thead>
+            </table>
+
             <CanvasJSChart options = {options} 
                 //onRef={ref => this.chart = ref}
             />
+
             <div className="col-6 mx-auto">
                 <label for="customRange3" class="form-label">Data Points</label>
                 <Form>
